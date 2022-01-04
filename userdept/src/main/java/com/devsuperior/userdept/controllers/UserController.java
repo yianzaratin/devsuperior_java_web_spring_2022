e6dde7d7447379fe35b4703451cc6ca854fd614e
Yian Zaratin <yian@uol.com.br>
2022-01-04 08:43:01 -0300

    Projeto Finalizado

--- userdept/src/main/java/com/devsuperior/userdept/controllers/UserController.java
+++ userdept/src/main/java/com/devsuperior/userdept/controllers/UserController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +25,17 @@ public class UserController {
 		List <User> result = repository.findAll();
 		return result;
 	}
+	
+	@GetMapping(value = "/{id}")
+	public User findById(@PathVariable Long id) {
+		User result = repository.findById(id).get();
+		return result;
+	}
+
+	@PostMapping
+	public User insert(@RequestBody User user) {
+		User result = repository.save(user); 
+		return result;
+	}
+	
 }
